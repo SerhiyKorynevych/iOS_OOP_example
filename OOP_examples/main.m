efd723019e890190f9c5cf5a5419339909fb7d10
Serhiy Korynevych (UA) <serhiy.korynevych@taboola.com>
2021-04-02 15:11:45 +0300

    Initial Commit

---
+++ OOP_examples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OOP_examples
+//
+//  Created by Serhiy Korynevych on 02.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
