76857295dc21fc58044ffad1890e5ff6ba578a70
Serhiy Korynevych (UA) <serhiy.korynevych@taboola.com>
2021-04-06 13:36:26 +0300

    first commit

---
+++ OOP_examples/House.h
@@ -0,0 +1,16 @@
+//
+//  House.h
+//  OOP_examples
+//
+//  Created by Serhiy Korynevych on 05.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface House : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OOP_examples/House.m
@@ -0,0 +1,12 @@
+//
+//  House.m
+//  OOP_examples
+//
+//  Created by Serhiy Korynevych on 05.04.2021.
+//
+
+#import "House.h"
+
+@implementation House
+
+@end
